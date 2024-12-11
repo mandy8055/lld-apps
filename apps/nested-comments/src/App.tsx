79ddc84c508abd121dbbaafa05cbfaca81ed066c
Manuj Sankrit <msankrit8055@gmail.com>
2024-12-11 10:00:31 +0530

    fix: :bug: fixed pipeline bug

--- apps/nested-comments/src/App.tsx
+++ apps/nested-comments/src/App.tsx
@@ -1,5 +1,5 @@
 const App: React.FC = () => {
-  return <h1 className='font-bold text-3xl'>Hello World!</h1>;
+  return <h1 className='font-bold text-3xl'>Hello World!!</h1>;
 };
 
 export default App;
